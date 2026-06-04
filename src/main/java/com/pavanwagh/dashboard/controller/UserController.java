fb26bb998af1bf745594e4b8a1f2ef753bb36984
Pavan Wagh <pawan.s.wagh@gmail.com>
2026-06-05 01:03:28 +0530

    feat: add email validation and normalization

--- src/main/java/com/pavanwagh/dashboard/controller/UserController.java
+++ src/main/java/com/pavanwagh/dashboard/controller/UserController.java
@@ -31,10 +31,15 @@ public class UserController {
 
     @PostMapping("/register")
     public String register (@RequestBody RegisterRequest request) {
-        if (userService.doesEmailExist(request.getEmail())) { return "Account already exists,log in to the account"; }
+        String email = request.getEmail();
+        if (email == null) return "Email field is Empty.";                            // Email null check
+        email = email.trim();                                                         // trim()
+        if (email.length() == 0) return "Email field is Empty, enter valid email.";   // Check after trim email length is 0 or not
+        email = email.toLowerCase();                                                  // Convert to Lowercase
+        if (userService.doesEmailExist(email)) { return "Account already exists,log in to the account"; }     // Check if email already exist
 
         userService.registration(
-                request.getEmail(),
+                email,
                 request.getPassword(),
                 request.getFullName(),
                 request.getDepartment(),
