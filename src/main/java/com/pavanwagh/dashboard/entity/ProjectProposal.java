2a44e1d01e8714256bf6c4ea3b0a80aca1319b10
Saurav2k05 <jamadarsaurav10@gmail.com>
2026-08-25 01:06:17 +0530

    (feat:backend)Contructors and getter setter are added in to entity ProjectProposal

--- src/main/java/com/pavanwagh/dashboard/entity/ProjectProposal.java
+++ src/main/java/com/pavanwagh/dashboard/entity/ProjectProposal.java
@@ -30,4 +30,72 @@ public class ProjectProposal {
     @Column(name = "proposal_status")
      private ProposalStatus proposalStatus ;
 
+    // Constructors
+    public ProjectProposal() { } // Default Constructor required JPA
+    public ProjectProposal(Long teamId, String title, String projectDescription, String pdfFileName, String pdfFilePath, ProposalStatus proposalStatus) {
+        this.teamId = teamId;
+        this.title = title;
+        this.projectDescription = projectDescription;
+        this.pdfFileName = pdfFileName;
+        this.pdfFilePath = pdfFilePath;
+        this.proposalStatus = proposalStatus;
+    }
+
+    public Long getProposalId() {
+        return proposalId;
+    }
+
+    public void setProposalId(Long proposalId) {
+        this.proposalId = proposalId;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public String getPdfFileName() {
+        return pdfFileName;
+    }
+
+    public void setPdfFileName(String pdfFileName) {
+        this.pdfFileName = pdfFileName;
+    }
+
+    public String getPdfFilePath() {
+        return pdfFilePath;
+    }
+
+    public void setPdfFilePath(String pdfFilePath) {
+        this.pdfFilePath = pdfFilePath;
+    }
+
+    public ProposalStatus getProposalStatus() {
+        return proposalStatus;
+    }
+
+    public void setProposalStatus(ProposalStatus proposalStatus) {
+        this.proposalStatus = proposalStatus;
+    }
+
+
 }
